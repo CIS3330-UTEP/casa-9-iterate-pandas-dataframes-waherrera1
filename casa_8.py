ece5d92bc43e3c66e7e162af8e8ecc2af5b651b5
waherrera1 <waherrera1@miners.utep.edu>
2025-02-22 00:14:52 -0700

    Casa 8
    Casa assignment

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,24 @@
+import pandas as pd
+file = open('./big-mac-full-index.csv')
+df = pd.read_csv(file)
+#df = pd.DataFrame(file)
 
+#In this code I use iterrows and apply to get the difference between dollar and local price for the location on the recorded date
+def iterrows():
+    for index, row in df.iterrows():
+        country_name = row['name']
+        price_diff = row['dollar_price'] - row['local_price']
+        year = row['date']
+        print(f'In {country_name}, the difference between dollar and local price on the date {year} was: ${price_diff}')
+
+def get_pricediff(row):
+    return(f'In {row['name']}, the difference between dollar and local price on the date {row['date']} was: ${row['dollar_price'] - row['local_price']}')
+           
+if __name__ == "__main__":
+    print('-----------Using the iterrows method-----------')
+    iterrows()
+    
+    print('-----------Using the apply method-----------')
+    result = df.apply(get_pricediff, axis=1)
+    for answer in result:
+        print(answer)
